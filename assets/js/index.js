c2ecb282f9f6bf9f29396a76f2f66db5dd865bd7
Skyz03 <aakibdiscoo@gmail.com>
2022-06-18 17:59:54 +0545

    Completed bits

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,18 @@
-// const cardDays = document.querySelector(".text");
+const cardDays = Array.from(document.querySelectorAll(".days"));
 
-// fetch("data.json")
-//   .then((response) => response.json())
-//   .then((data) => {
-//     data;
-//     const days = data
-//       .map((day) => {
-//         cardDays.innerHTML = day.day;
-//       })
-//       .join("");
-//   });
-// // console.log(data);;
+fetch("data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    data;
+    const days = data
+      .map((day) => {
+        for (let i = 0; i <= cardDays.length; i++) {
+          console.log(i);
+          console.log(cardDays.length);
+          cardDays[i].innerHTML = day.day;
+        }
+        console.log(day.day);
+      })
+      .join("");
+    // console.log(days);
+  });
